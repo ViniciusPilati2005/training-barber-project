3cc89efed7ca27ea800ef5614bf13f0af2cf6687
Vinicius Raitz Pilati <vini1718a@gmail.com>
2024-09-03 23:57:38 -0300

    Tratando erros do firebase

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,11 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { Toaster } from "@/components/ui/toaster";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
     <>
+      <Toaster />
       <Outlet />
       <TanStackRouterDevtools />
     </>

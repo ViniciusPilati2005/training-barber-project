e0f4630f4c0ff3edd931ef99c126f158c344e7e9
Vinicius Raitz Pilati <vini1718a@gmail.com>
2024-09-07 11:58:39 -0300

    Adicionando return no root(isLogged), para evitar loop, pois toda vez que redireciona, as rotas são recarregadas

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,7 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet, redirect } from "@tanstack/react-router";
 import { Toaster } from "@/components/ui/toaster";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { auth } from "@/utils/firebase";
 
 export const Route = createRootRoute({
   component: () => (
@@ -10,4 +11,14 @@ export const Route = createRootRoute({
       <TanStackRouterDevtools />
     </>
   ),
+  beforeLoad: async () => {
+    await auth.authStateReady();
+
+    const user = auth.currentUser;
+    const isLogged = !!user;
+
+    if (!isLogged) return;
+
+    if (!user) throw redirect({ to: "/" });
+  },
 });

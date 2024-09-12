c758e456ff7a682f6289b8df46fb1eb514179687
Vinicius Raitz Pilati <vini1718a@gmail.com>
2024-09-12 00:03:12 -0300

    Validando e fazendo navegações dependendo da rota, e estado(logado, não logado) que o usuário estiver, rota acessada usando location

---
+++ src/constants/routes.ts
@@ -0,0 +1,6 @@
+export const ROUTE_LOGIN='/'
+export const ROUTE_MAIN='/dashboard'
+
+export const ROUTE_LOGIN_REGEX=new RegExp(/\/$/)
+export const ROUTE_MAIN_REGEX=new RegExp(/^\/dashboard\/?$/)
+
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,6 +2,12 @@ import { createRootRoute, Outlet, redirect } from "@tanstack/react-router";
 import { Toaster } from "@/components/ui/toaster";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { auth } from "@/utils/firebase";
+import {
+  ROUTE_LOGIN,
+  ROUTE_LOGIN_REGEX,
+  ROUTE_MAIN,
+  ROUTE_MAIN_REGEX,
+} from "@/constants/routes";
 
 export const Route = createRootRoute({
   component: () => (
@@ -11,14 +17,21 @@ export const Route = createRootRoute({
       <TanStackRouterDevtools />
     </>
   ),
-  beforeLoad: async () => {
+  beforeLoad: async ({ location }) => {
     await auth.authStateReady();
 
     const user = auth.currentUser;
     const isLogged = !!user;
+    const route = location.pathname;
+
+    if (!isLogged && ROUTE_MAIN_REGEX.test(route)) {
+      throw redirect({ to: ROUTE_LOGIN });
+    }
 
     if (!isLogged) return;
 
-    if (!user) throw redirect({ to: "/" });
+    if (ROUTE_LOGIN_REGEX.test(route) && isLogged) {
+      throw redirect({ to: ROUTE_MAIN });
+    }
   },
 });
